fa5db4fb5120b80ca36bcd9ea405d59fee379125
ThomasLlopiz <toomillopiz@gmail.com>
2021-09-20 16:53:33 -0300

    se agrego duenos

--- nodejs/enrutador.js
+++ nodejs/enrutador.js
@@ -1,12 +1,14 @@
 const recursos = require("./recursos");
 const mascotas = require("./rutas/mascotas");
 const veterinarias = require("./rutas/veterinarias");
+const duenos = require("./rutas/duenos");
 module.exports = {
   ruta: (data, callback) => {
     callback(200, { mensaje: "esta es /ruta" });
   },
   mascotas: mascotas(recursos.mascotas),
   veterinarias: veterinarias(recursos.veterinarias),
+  duenos: duenos(recursos.duenos),
   noEncontrado: (data, callback) => {
     callback(404, { mensaje: "no encontrado" });
   }
