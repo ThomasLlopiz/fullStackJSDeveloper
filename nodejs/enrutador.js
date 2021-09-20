e9ea410bc28466103e20c161a2b1c6bbac77b958
ThomasLlopiz <toomillopiz@gmail.com>
2021-09-20 15:56:11 -0300

    se agrego veterinarias

--- nodejs/enrutador.js
+++ nodejs/enrutador.js
@@ -1,10 +1,12 @@
 const recursos = require("./recursos");
 const mascotas = require("./rutas/mascotas");
+const veterinarias = require("./rutas/veterinarias");
 module.exports = {
   ruta: (data, callback) => {
     callback(200, { mensaje: "esta es /ruta" });
   },
   mascotas: mascotas(recursos.mascotas),
+  veterinarias: veterinarias(recursos.veterinarias),
   noEncontrado: (data, callback) => {
     callback(404, { mensaje: "no encontrado" });
   }
