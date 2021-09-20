e68f662ae69782d01e976d908ed428e4b411fd8d
ThomasLlopiz <toomillopiz@gmail.com>
2021-09-20 15:56:03 -0300

    se agrego veterinarias

--- nodejs/recursos.js
+++ nodejs/recursos.js
@@ -5,5 +5,12 @@ module.exports = {
         { tipo: "perro", nombre: "Trosky2", dueno: "Camilo" },
         { tipo: "perro", nombre: "Trosky3", dueno: "Camilo" },
         { tipo: "perro", nombre: "Trosky4", dueno: "Camilo" }
-      ]
+      ],
+      veterinarias:
+      [
+        { nombre: "Alexandra", apellido: "Perez", documento: "1234567890" },
+        { nombre: "Alexder", apellido: "Gomez", documento: "9999999999" },
+        { nombre: "Natalia", apellido: "Vazques", documento: "7777777777" },
+      ]  
+
   }
