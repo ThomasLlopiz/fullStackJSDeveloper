8610e1eb5cc09a3f0a6352910317e81e593cd228
ThomasLlopiz <toomillopiz@gmail.com>
2021-09-20 12:00:04 -0300

    se creo metodo delete

--- nodejs/enrutador.js
+++ nodejs/enrutador.js
@@ -1,37 +1,53 @@
 module.exports = {
-    ruta: (data, callback) => {
-      callback(200, { mensaje: "esta es /ruta" });
+  ruta: (data, callback) => {
+    callback(200, { mensaje: "esta es /ruta" });
+  },
+  mascotas: {
+    get: (data, callback) => {
+      if (data.indice) {
+        if (global.recursos.mascotas[data.indice]) {
+          return callback(200, global.recursos.mascotas[data.indice]);
+        }
+        return callback(404, {
+          mensaje: `mascota con indice ${data.indice} no encontrada`
+        });
+      }
+      callback(200, global.recursos.mascotas);
     },
-    mascotas: {
-      get: (data, callback) => {
-        if (data.indice) {
-          if (global.recursos.mascotas[data.indice]) {
-            return callback(200, global.recursos.mascotas[data.indice]);
-          }
-          return callback(404, { 
-            mensaje: `mascota con indice ${data.indice} no encontrada` 
+    post: (data, callback) => {
+      global.recursos.mascotas.push(data.payload);
+      callback(201, data.payload);
+    },
+    put: (data, callback) => {
+      if (typeof data.indice !== "undefined") {
+        if (global.recursos.mascotas[data.indice]) {
+          global.recursos.mascotas[data.indice] = data.payload;
+          return callback(200, global.recursos.mascotas[data.indice]);
+        }
+        return callback(404, {
+          mensaje: `mascota con indice ${data.indice} no encontrada`
+        });
+      }
+      callback(400, { mensaje: 'indice no enviado' });
+    },
+    delete: (data, callback) => {
+      if (typeof data.indice !== "undefined") {
+        if (global.recursos.mascotas[data.indice]) {
+          global.recursos.mascotas = global.recursos.mascotas.filter(
+            (_mascota, indice) => indice != data.indice
+          );
+          return callback(204, {
+            mensaje: `elemento con indice ${data.indice} eliminado`
           });
         }
-        callback(200, global.recursos.mascotas);
-      },
-      post: (data, callback) => {
-        global.recursos.mascotas.push(data.payload);
-        callback(201, data.payload);
-      },
-      put: (data, callback) => {
-        if (typeof data.indice !== "undefined") {
-          if (global.recursos.mascotas[data.indice]) {
-            global.recursos.mascotas[data.indice] = data.payload;
-            return callback(200, global.recursos.mascotas[data.indice]);
-          }
-          return callback(404, { 
-            mensaje: `mascota con indice ${data.indice} no encontrada` 
-          });
-        }
-        callback(400, {mensaje: 'indice no enviado'});
-      },
+        return callback(404, {
+          mensaje: `mascota con indice ${data.indice} no encontrada`
+        });
+      }
+      callback(400, { mensaje: 'indice no enviado' });
     },
-    noEncontrado: (data, callback) => {
-      callback(404, { mensaje: "no encontrado" });
-    }
+  },
+  noEncontrado: (data, callback) => {
+    callback(404, { mensaje: "no encontrado" });
   }
+}
