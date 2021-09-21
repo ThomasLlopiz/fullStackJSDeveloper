9f27387688c5d99d8eee8ebc7056013f97ec31c9
ThomasLlopiz <toomillopiz@gmail.com>
2021-09-21 15:03:22 -0300

    se agrego consultas

--- nodejs/enrutador.js
+++ nodejs/enrutador.js
@@ -2,6 +2,7 @@ const recursos = require("./recursos");
 const mascotas = require("./rutas/mascotas");
 const veterinarias = require("./rutas/veterinarias");
 const duenos = require("./rutas/duenos");
+const consultas = require ("./rutas/consultas");
 module.exports = {
   ruta: (data, callback) => {
     callback(200, { mensaje: "esta es /ruta" });
@@ -9,6 +10,7 @@ module.exports = {
   mascotas: mascotas(recursos.mascotas),
   veterinarias: veterinarias(recursos.veterinarias),
   duenos: duenos(recursos.duenos),
+  consultas: consultas(recursos.consultas),
   noEncontrado: (data, callback) => {
     callback(404, { mensaje: "no encontrado" });
   }
