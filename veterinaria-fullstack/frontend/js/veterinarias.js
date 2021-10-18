778363f64ef168b8c8d76dd556458d0a4ccf5d87
ThomasLlopiz <toomillopiz@gmail.com>
2021-10-18 14:57:19 -0300

    listar datos

--- veterinaria-fullstack/frontend/js/veterinarias.js
+++ veterinaria-fullstack/frontend/js/veterinarias.js
@@ -7,95 +7,100 @@ const indice = document.getElementById('indice');
 const form = document.getElementById('form');
 const exampleModal = document.getElementById('exampleModal');
 const btnGuardar = document.getElementById('btn-guardar');
-let veterinarias = [
-    {
-        pais: "Ecuador",
-        identificacion: "123456789",        
-        nombre: "Esteban",
-        apellido: "Alberto",               
-    },
-    {
-        pais: "Argentina",
-        identificacion: "123456789",
-        nombre: "Thomas",
-        apellido: "Llopiz",
-    },
-]
+const alert = document.getElementById("alert");
+const url = "http://localhost:5000/veterinarias";
+let veterinarias = []
 
-function listarVeterinarias() {
-    const htmlVeterinarias = veterinarias.map((veterinaria,index) => `
-    <tr>
-    <th scope="row">${index}</th>
-    <td>${veterinaria.identificacion}</td>
-    <td>${veterinaria.pais}</td>
-    <td>${veterinaria.nombre}</td>
-    <td>${veterinaria.apellido}</td>
-    <td>
-        <div class="btn-group" role="group" aria-label="Basic example">
-            <button type="button" class="btn btn-info editar" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fas fa-edit"></i></button>
-            <button type="button" class="btn btn-danger eliminar"><i class="far fa-trash-alt"></i></button>
-        </div>
-    </td>
-    </tr>
-    `).join('');
-    listaVeterinarias.innerHTML = htmlVeterinarias;
-    Array.from(document.getElementsByClassName('editar')).forEach((bontonEditar,index)=>bontonEditar.onclick = editar(index));
-    Array.from(document.getElementsByClassName('eliminar')).forEach((bontonEliminar,index)=>bontonEliminar.onclick = eliminar(index));
-}
-
-function enviarDatos(e) {
-    e.preventDefault();
-    const datos = {
-        pais: pais.value,
-        identificacion: identificacion.value,
-        nombre: nombre.value,
-        apellido: apellido.value,
-    };
-    const accion = btnGuardar.innerHTML;
-    switch (accion) {
-        case 'Editar':
-            veterinarias[indice.value] = datos;
-        break;
-
-        default:
-        //Crear
-            veterinarias.push(datos);
-        break;
-    }
-    listarVeterinarias();
-    resetModal();
-}
-
-function editar(index){
-    return function cuandoHagoClick(){
-        btnGuardar.innerHTML = 'Editar'
-        const veterinaria = veterinarias[index];
-        pais.value = veterinaria.pais;
-        identificacion.value = veterinaria.identificacion;
-        nombre.value = veterinaria.nombre;
-        apellido.value = veterinaria.apellido;
-        indice.value = index;
+async function listarVeterinarias() {
+    try {
+        const respuesta = await fetch(url);
+        const veterinariasDelServer = await respuesta.json();
+        if (Array.isArray(veterinariasDelServer)) {
+            veterinarias = veterinariasDelServer;
+        }
+        if (veterinarias.length > 0) {
+            const htmlVeterinarias = veterinarias
+            .map((veterinaria, index) => `
+            <tr>
+            <th scope="row">${index}</th>
+            <td>${veterinaria.nombre}</td>
+            <td>${veterinaria.apellido}</td>
+            <td>${veterinaria.documento}</td>
+            <td>
+                <div class="btn-group" role="group" aria-label="Basic example">
+                    <button type="button" class="btn btn-info editar" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fas fa-edit"></i></button>
+                    <button type="button" class="btn btn-danger eliminar"><i class="far fa-trash-alt"></i></button>
+                </div>
+            </td>
+            </tr>
+            `).join('');
+            listaVeterinarias.innerHTML = htmlVeterinarias;
+            Array.from(document.getElementsByClassName('editar')).forEach((bontonEditar, index) => bontonEditar.onclick = editar(index));
+            Array.from(document.getElementsByClassName('eliminar')).forEach((bontonEliminar, index) => bontonEliminar.onclick = eliminar(index));
+            return;
+        }
+        listaVeterinarias.innerHTML =`<tr>
+                                        <td colspan="5">No hay mascotas</td>
+                                      </tr>`;
+    } catch (error) {
+        console.log({ error });
+        // $(alert).show();
     }
 }
 
-function resetModal(){
-    indice.value = '';
-    pais.value = '';
-    identificacion.value = '';
-    nombre.value = '';
-    apellido.value = '';
-    btnGuardar.innerHTML = 'Crear'
-}
+    function enviarDatos(e) {
+        e.preventDefault();
+        const datos = {
+            pais: pais.value,
+            identificacion: identificacion.value,
+            nombre: nombre.value,
+            apellido: apellido.value,
+        };
+        const accion = btnGuardar.innerHTML;
+        switch (accion) {
+            case 'Editar':
+                veterinarias[indice.value] = datos;
+                break;
 
-function eliminar (index){
-    return  function clickEnEliminar () {
-        console.log(index);
-        veterinarias = veterinarias.filter((veterinaria,indiceVeterinaria)=>indiceVeterinaria !== index);
+            default:
+                //Crear
+                veterinarias.push(datos);
+                break;
+        }
         listarVeterinarias();
+        resetModal();
     }
-}
 
-listarVeterinarias();
+    function editar(index) {
+        return function cuandoHagoClick() {
+            btnGuardar.innerHTML = 'Editar'
+            const veterinaria = veterinarias[index];
+            pais.value = veterinaria.pais;
+            identificacion.value = veterinaria.identificacion;
+            nombre.value = veterinaria.nombre;
+            apellido.value = veterinaria.apellido;
+            indice.value = index;
+        }
+    }
 
-form.onsubmit = enviarDatos;
-btnGuardar.onclick = enviarDatos;
+    function resetModal() {
+        indice.value = '';
+        pais.value = '';
+        identificacion.value = '';
+        nombre.value = '';
+        apellido.value = '';
+        btnGuardar.innerHTML = 'Crear'
+    }
+
+    function eliminar(index) {
+        return function clickEnEliminar() {
+            console.log(index);
+            veterinarias = veterinarias.filter((veterinaria, indiceVeterinaria) => indiceVeterinaria !== index);
+            listarVeterinarias();
+        }
+    }
+
+    listarVeterinarias();
+
+    form.onsubmit = enviarDatos;
+    btnGuardar.onclick = enviarDatos;
