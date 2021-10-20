41c5f0e47c1d66017f78be7a6c3857eb2aa4d97e
ThomasLlopiz <toomillopiz@gmail.com>
2021-10-20 09:20:22 -0300

    listar consultas

--- veterinaria-fullstack/frontend/js/consultas.js
+++ veterinaria-fullstack/frontend/js/consultas.js
@@ -6,89 +6,39 @@ const indice = document.getElementById('indice');
 const form = document.getElementById('form');
 const exampleModal = document.getElementById('exampleModal');
 const btnGuardar = document.getElementById('btn-guardar');
-let consultas = [
-    {   
-        nombre: "Esteban",
-        apellido: "Alberto",  
-        email: "@algo",             
-    },
-    {
-        nombre: "Thomas",
-        apellido: "Llopiz",
-        email: "@tomi",
-    },
-]
+const url = "http://localhost:5000/consultas";
+let consultas = [];
 
-function listarConsultas() {
-    const htmlConsultas = consultas.map((consulta,index) => `
-    <tr>
-    <th scope="row">${index}</th>
-    <td>${consulta.nombre}</td>
-    <td>${consulta.apellido}</td>
-    <td>${consulta.email}</td>
-    <td>
-        <div class="btn-group" role="group" aria-label="Basic example">
-            <button type="button" class="btn btn-info editar" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fas fa-edit"></i></button>
-            <button type="button" class="btn btn-danger eliminar"><i class="far fa-trash-alt"></i></button>
-        </div>
-    </td>
-    </tr>
-    `).join('');
-    listaConsultas.innerHTML = htmlConsultas;
-    Array.from(document.getElementsByClassName('editar')).forEach((bontonEditar,index)=>bontonEditar.onclick = editar(index));
-    Array.from(document.getElementsByClassName('eliminar')).forEach((bontonEliminar,index)=>bontonEliminar.onclick = eliminar(index));
-}
-
-function enviarDatos(e) {
-    e.preventDefault();
-    const datos = {
-        nombre: nombre.value,
-        apellido: apellido.value,
-        email: email.value,
-    };
-    const accion = btnGuardar.innerHTML;
-    switch (accion) {
-        case 'Editar':
-            consultas[indice.value] = datos;
-        break;
-
-        default:
-        //Crear
-            consultas.push(datos);
-        break;
-    }
-    listarConsultas();
-    resetModal();
-}
-
-function editar(index){
-    return function cuandoHagoClick(){
-        btnGuardar.innerHTML = 'Editar'
-        const consulta = consultas[index];
-        nombre.value = consulta.nombre;
-        apellido.value = consulta.apellido;
-        email.value = consulta.email;
-        indice.value = index;
-    }
-}
-
-function resetModal(){
-    indice.value = '';
-    nombre.value = '';
-    apellido.value = '';
-    email.value = '';
-    btnGuardar.innerHTML = 'Crear'
-}
-
-function eliminar (index){
-    return  function clickEnEliminar () {
-        console.log(index);
-        consultas = consultas.filter((veterinaria,indiceConsultas)=>indiceConsultas !== index);
-        listarConsultas();
+async function listarConsultas() {
+    try {
+        const respuesta = await fetch(url);
+        const consultasDelServidor = await respuesta.json();
+        if (Array.isArray(consultasDelServidor)) {
+            consultas = consultasDelServidor;
+        }
+        if (respuesta.ok) {
+            const htmlConsultas = consultas.map(
+                (consulta, indice) =>
+            `<tr>
+                <th scope="row">${indice}</th>
+                <td>${consulta.mascota.nombre}</td>
+                <td>${consulta.veterinaria.nombre} ${consulta.veterinaria.apellido}</td>
+                <td>${consulta.diagnostico}</td>
+                <td>${consulta.fechaCreacion}</td>
+                <td>${consulta.fechaEdicion}</td>
+                <td>${consulta.historia}</td>
+                <td>
+                    <div class="btn-group" role="group" aria-label="Basic example">
+                        <button type="button" class="btn btn-info editar" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fas fa-edit"></i></button>
+                    </div>
+                </td>
+            </tr>`).join("");
+            listaConsultas.innerHTML = htmlConsultas;
+        }
+    } catch (error) {
+        throw error;
     }
 }
 
 listarConsultas();
 
-form.onsubmit = enviarDatos;
-btnGuardar.onclick = enviarDatos;
