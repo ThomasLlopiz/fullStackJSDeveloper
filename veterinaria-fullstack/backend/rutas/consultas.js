10e0f766999b09b6e1cab9d4ba59652ad8a61399
ThomasLlopiz <toomillopiz@gmail.com>
2021-10-22 10:51:19 -0300

    se le agrego id  mascota y veterinaria

--- veterinaria-fullstack/backend/rutas/consultas.js
+++ veterinaria-fullstack/backend/rutas/consultas.js
@@ -15,8 +15,8 @@ module.exports = function consultasHandler({
             }
             const consultasConRelaciones = consultas.map((consulta)=>(
             {...consulta,
-                mascota: mascotas[consulta.mascota],
-                veterinaria: veterinarias[consulta.veterinaria],
+                mascota:{ ...mascotas[consulta.mascota], id: consulta.mascota },
+                veterinaria: { ...veterinarias[consulta.veterinaria], id: consulta.veterinaria },
             }));
             callback(200, consultasConRelaciones);
         },
